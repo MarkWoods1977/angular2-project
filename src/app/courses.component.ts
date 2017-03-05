6750a3819d0619e2b7ed2e09f4ebbfaa3a579481
Mark Woods <markwoods1977@googlemail.com>
2017-03-05 20:20:10 +0000

    Added iteration of array

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: `
+      <h2>Courses</h2>
+      {{title}}
+      <ul>
+      <li *ngFor="let course of courses">
+      {{course}}
+</li>
+</ul>`
 })
 export class CoursesComponent {
-
+  title: string = "This title of the courses page";
+  courses = ["Course1", "Course2", "Course3"]
 }
